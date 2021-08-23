da780df6abd3491e1614312b0bb32edc621689c3
thanvanlong <72558875+thanvanlong@users.noreply.github.com>
2021-08-23 15:56:02 +0700

    config security

---
+++ src/main/java/com/example/social_network/entity/user/UserRole.java
@@ -0,0 +1,6 @@
+package com.example.social_network.entity.user;
+
+public enum UserRole {
+    USER_ROLE,
+    ADMIN_ROlE
+}
---
+++ src/main/java/com/example/social_network/entity/user/UserService.java
@@ -0,0 +1,34 @@
+package com.example.social_network.entity.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+    private final String USER_NOT_FOUND =
+            "User with email %s not found";
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(()->
+                        new UsernameNotFoundException(String.format(USER_NOT_FOUND,email)));
+    }
+
+    public String signup(User user){
+        boolean userExist = userRepository.findByEmail(user.getEmail()).isPresent();
+        if(userExist){
+            throw new IllegalStateException("Email already token");
+        }
+
+
+        return "";
+    }
+}
